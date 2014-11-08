a28c0a211510c6fe9fef516ab930b30e41a90831
jespinozam71181 <julioemilio@yahoo.es>
2014-11-07 22:27:44 -0600

    #5 creación de tabla auxiliar de lotes

--- LotesExactus/Batch.sql
+++ LotesExactus/Batch.sql
@@ -590,3 +590,35 @@ begin
 	
 END
 
+GO 
+
+--#4 Creacion de Tabla temporal para almacenar lotes
+CREATE TABLE [fnica].[tmpLotesAsignados](
+	[Fecha] DATETIME,
+	[TipoDocumento] [NVARCHAR] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Documento] [nvarchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Bodega] [varchar](4) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[BodegaDestino] [varchar](4) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Articulo] [varchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[Lote] [nvarchar] (15) NOT NULL,
+	[TipoTran] [int] NOT NULL,
+	[CantidadLote] [numeric](28, 8) NULL,
+	[Cantidad] [numeric](28, 8) NULL,
+ CONSTRAINT [PK_tmpLotesAsignados] PRIMARY KEY CLUSTERED 
+(
+	[Fecha] ASC,
+	[TipoDocumento] ASC,
+	[Documento] ASC,
+	[Bodega] ASC,
+	[BodegaDestino] ASC,
+	[Articulo] ASC,
+	[Lote] ASC
+	
+)WITH (IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+
+GO 
+
+--
