f748341a5fc0c79e4d9d40531dafdc65e166808a
jespinoza711 <julioemilio2003@yahoo.es>
2014-11-17 16:44:33 -0600

    #10 Cambios Insert Lotes Asignados

--- LotesExactus/Batch.sql
+++ LotesExactus/Batch.sql
@@ -833,3 +833,13 @@ ORDER BY EL.FECHA_VENCIMIENTO ASC
 
 GO 
 
+CREATE PROCEDURE [fnica].[usp_invInsertLotesAsignados] @Fecha DATETIME,@TipoDocumento NVARCHAR(50),
+				@Documento NVARCHAR(50),@Bodega NVARCHAR(10),@BodegaDestino NVARCHAR(10),@Articulo NVARCHAR(20),
+				@Lote NVARCHAR(15),@TipoTran NVARCHAR(1),@CantidadLote DECIMAL(28,8),@Cantidad DECIMAL(28,8)
+AS 
+
+DELETE FROM fnica.tmpLotesAsignados WHERE Documento=@Documento
+
+INSERT INTO fnica.tmpLotesAsignados(Fecha, TipoDocumento, Documento, Bodega,
+            BodegaDestino, Articulo, Lote, TipoTran, CantidadLote, Cantidad)
+VALUES (@Fecha,@TipoDocumento,@Documento,@Bodega,@BodegaDestino,@Articulo,@Lote,@TipoTran,@CantidadLote,@Cantidad)
